d72056f09c89de41dd242978eb60c0e930eb3a0e
Yewei Song <syw12345ht@gmail.com>
2018-06-08 15:49:12 +0800

    jwt
    JWT

--- src/main/java/com/pginfo/datacollect/service/UserService.java
+++ src/main/java/com/pginfo/datacollect/service/UserService.java
@@ -1,5 +1,19 @@
 package com.pginfo.datacollect.service;
+import org.springframework.stereotype.Component;
+import java.util.Map;
 
 public class UserService {
+    public UserBean getUser(String username) {
+        // if not exist return null
+        if (! DataSource.getData().containsKey(username))
+            return null;
 
+        UserBean user = new UserBean();
+        Map<String, String> detail = DataSource.getData().get(username);
+
+        user.setUsername(username);
+        user.setPassword(detail.get("password"));
+        user.setRole(detail.get("role"));
+        return user;
+    }
 }
