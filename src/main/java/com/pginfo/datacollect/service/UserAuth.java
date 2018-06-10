8984abbf4b649f6e77bc5c6c640c3cd7f6435749
Etamin <syw12345ht@gmail.com>
2018-06-10 22:30:55 +0800

    auth

--- src/main/java/com/pginfo/datacollect/service/UserAuth.java
+++ src/main/java/com/pginfo/datacollect/service/UserAuth.java
@@ -2,6 +2,9 @@ package com.pginfo.datacollect.service;
 
 import java.util.HashMap;
 import java.util.Map;
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoDatabase;
+
 
 public class UserAuth {
     private static Map<String, Map<String, String>> data = new HashMap<>();
@@ -10,18 +13,27 @@ public class UserAuth {
         Map<String, String> data1 = new HashMap<>();
         data1.put("password", "smith123");
         data1.put("role", "user");
-        data1.put("permission", "view");
+
 
         Map<String, String> data2 = new HashMap<>();
         data2.put("password", "danny123");
         data2.put("role", "admin");
-        data2.put("permission", "view,edit");
 
         data.put("smith", data1);
         data.put("danny", data2);
     }
 
     public static Map<String, Map<String, String>> getData() {
+        try{
+            MongoClient mongoClient = new MongoClient( "localhost" , 27017 );
+            MongoDatabase mongoDatabase = mongoClient.getDatabase("userdb");
+            System.out.println("Connect to database successfully");
+        }catch(Exception e){
+            System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+        }
+        
+
+
         return data;
     }
 }
