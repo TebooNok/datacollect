06966e88c9e2a90ecd19645de828fb93e7bd950c
Etamin <syw12345ht@gmail.com>
2018-06-17 13:49:13 +0800

    manageruser

--- src/main/java/com/pginfo/datacollect/controller/ManageUserController.java
+++ src/main/java/com/pginfo/datacollect/controller/ManageUserController.java
@@ -1,8 +1,11 @@
 package com.pginfo.datacollect.controller;
 
 
-
+import com.pginfo.datacollect.dto.ManageDeviceResponse;
+import com.pginfo.datacollect.dto.ManagerUserRequest;
+import com.pginfo.datacollect.dto.ManagerUserResponse;
 import com.pginfo.datacollect.service.UserService;
+import com.pginfo.datacollect.util.Constants;
 import org.apache.shiro.authz.annotation.RequiresRoles;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -20,6 +23,47 @@ public class ManageUserController {
     }
 
     //TODO: 3 interfaces
+    @GetMapping("/add_user")
+    @RequiresRoles("admin")
+    public ManagerUserResponse addUser(ManagerUserRequest request) {
 
+        try{
+            userService.AddUser(request.getUsername(),request.getPassword(),request.getRole());
+        }
+        catch (Exception e){
+            return new ManagerUserResponse(Constants.INTERNAL_ERROR_CODE, "[Fatal Error in add user progress]" + e.getMessage(), null);
+        }
+
+        return new ManagerUserResponse(Constants.SUCCESS_CODE, Constants.SUCCESS_MSG, null);
+    }
+
+
+    @GetMapping("/update_user")
+    @RequiresRoles("admin")
+    public ManagerUserResponse updateUser(ManagerUserRequest request) {
+
+        try{
+            userService.UpdateUser(request.getId(),request.getUsername(),request.getPassword(),request.getRole());
+        }
+        catch (Exception e){
+            return new ManagerUserResponse(Constants.INTERNAL_ERROR_CODE, "[Fatal Error in update user info progress]" + e.getMessage(), null);
+        }
+
+        return new ManagerUserResponse(Constants.SUCCESS_CODE, Constants.SUCCESS_MSG, null);
+    }
+
+    @GetMapping("/delete_user")
+    @RequiresRoles("admin")
+    public ManagerUserResponse deleteUser(ManagerUserRequest request) {
+
+        try{
+            userService.deleteUser(request.getUsername());
+        }
+        catch (Exception e){
+            return new ManagerUserResponse(Constants.INTERNAL_ERROR_CODE, "[Fatal Error in delete user progress]" + e.getMessage(), null);
+        }
+
+        return new ManagerUserResponse(Constants.SUCCESS_CODE, Constants.SUCCESS_MSG, null);
+    }
 
 }
---
+++ src/main/java/com/pginfo/datacollect/dto/ManagerUserResponse.java
@@ -0,0 +1,7 @@
+package com.pginfo.datacollect.dto;
+
+public class ManagerUserResponse extends ResponseBean{
+    public ManagerUserResponse(int code, String msg, Object data) {
+        super(code, msg, data);
+    }
+}
